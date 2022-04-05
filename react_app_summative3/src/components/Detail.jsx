9367b23c0d4fe6131b86d168f42b6a8c4694e5c3
lynn <270146689@yoobeestudent.ac.nz>
2022-04-06 11:58:48 +1200

    make detail tab work

--- react_app_summative3/src/components/Detail.jsx
+++ react_app_summative3/src/components/Detail.jsx
@@ -1,92 +1,160 @@
-import React from 'react';
-import '../css/postPage.css';
-import Backbtn from '../components/Backbtn.jsx'
-import Infobtn from '../components/Infobtn.jsx'
-import Commentbtn from '../components/Commentbtn.jsx'
+import React from "react";
+import "../css/postPage.css";
+import Backbtn from "../components/Backbtn.jsx";
+import mike from "../img/mike.jpg";
+import Avatar from "@mui/material/Avatar";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+
+import PhoneIcon from "@mui/icons-material/Phone";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import PropTypes from "prop-types";
+import SwipeableViews from "react-swipeable-views";
+import { useTheme } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Typography from "@mui/material/Typography";
+//  the tabbar SwipeableViews start
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `full-width-tab-${index}`,
+    "aria-controls": `full-width-tabpanel-${index}`,
+  };
+}
+//  the tabbar SwipeableViews   end
 
 function Detail(props) {
   console.log(props);
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleChangeIndex = (index) => {
+    setValue(index);
+  };
+
   return (
     <>
       <div className="postPage">
-            <div className="title">
-              <a href="">
-                < Backbtn />
-              </a>
-              <h2>Animal Title</h2>
-            </div>
+        <div className="title">
+          <a href="">
+            <Backbtn />
+          </a>
+          <h2>Animal Title</h2>
+        </div>
 
-            <div className="postDetail">
-              <div className="detailImg"> </div>
-              <div className="postMenu">
-                <>
-                  <p>
-                    <span>username</span>
-                  </p>
-                  <p>post date</p>
-                </>
-              </div>
-
-              <div className="btnDetail">
-              
-                <a href=""> <Infobtn  /> </a>
-                <a href=""> <Commentbtn /> </a>
-              </div>
-
-              <div className='postDescription'>
-                <h3>Description</h3>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero unde hic nesciunt quis asperiores voluptate officia vitae corrupti rem. Perferendis iure voluptas quod omnis magnam libero dolorum dolores aliquam, accusamus provident ipsum odit alias veniam recusandae expedita possimus dicta. Ad in numquam alias, aliquam veritatis ducimus rem deserunt corporis exercitationem.</p>
-              </div>
-
-              <div className='comments'>
-                
-                <div className="profileImg profileImgS"></div>
-                <input type="text" placeholder='Write a comment...' className='postComment' />
-                <a href="">Post</a>
-              </div>
-
-              <div className='commentsDone'>
-                
-                <div className="profileImg profileImgS"></div>
-                <div id="comment-box" className='postComment'><p>
-                Where did you spot that bird? <br />beautiful creature
-                updating
+        <div className="postDetail">
+          <div className="detailImg"> </div>
+          <div className="postMenu">
+            <>
+              <p>
+                <span>username</span>
+              </p>
+              <p>post date</p>
+            </>
+          </div>
+          {/* ================tab bar========================= */}
+          <Box
+            sx={{
+              width: "100%",
+              margin: "20px 0px",
+              "& .MuiPaper-root": {
+                backgroundColor: "white",
+                boxShadow:'none',
+          
+              },
+             
+            }}
+          >
+            <AppBar position="static">
+              <Tabs
+                className="detail-tab"
+                value={value}
+                onChange={handleChange}
+                variant="fullWidth"
+                aria-label="full width tabs example"
+              >
+                <Tab
+                  icon={<PhoneIcon />}
+                  aria-label="Item One"
+                  {...a11yProps(0)}
+                />
+                <Tab icon={<FavoriteIcon />} {...a11yProps(1)} />
+              </Tabs>
+            </AppBar>
+            <SwipeableViews
+              axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+              index={value}
+              onChangeIndex={handleChangeIndex}
+            >
+              <TabPanel value={value} index={0} dir={theme.direction}>
+                <h3 className="tab-title">Description</h3>
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero
+                  unde hic nesciunt quis asperiores voluptate officia vitae
+                  corrupti rem. Perferendis iure voluptas quod omnis magnam
+                  libero dolorum dolores aliquam, accusamus provident ipsum odit
+                  alias veniam recusandae expedita possimus dicta. Ad in numquam
+                  alias, aliquam veritatis ducimus rem deserunt corporis
+                  exercitationem.
                 </p>
+              </TabPanel>
+
+              {/* ===========================comments =========================== */}
+              <TabPanel value={value} index={1} dir={theme.direction}>
+                <div className="comments">
+                  <Avatar alt="Mike" src={mike} />
+
+                  <input
+                    type="text"
+                    placeholder="Write a comment..."
+                    className="postComment"
+                  />
+                  <a href=" ">Post</a>
                 </div>
-
-                
-              </div>
-
-              <div className='commentsDone'>
-                
-                <div className="profileImg profileImgS"></div>
-                <div id="comment-box" className='postComment'><p>
-                Where did you spot that bird? <br />beautiful creature
-                </p>
+                <div className="commentsDone">
+                  <Avatar alt="Mike" src={mike} />
+                  <div id="comment-box" className="postComment">
+                    <p>
+                      Where did you spot that bird? <br />
+                      beautiful creature updating
+                    </p>
+                  </div>
                 </div>
-
-                
-              </div>
-
-            </div>
-
-      
-
-
+              </TabPanel>
+            </SwipeableViews>
+          </Box>
+        </div>
       </div>
-
-      {/* <h3> This is post  page</h3>
-     <h3> Task </h3>
-     <ol>
-       <li>get this post infor from DB and display </li>
-       <li>get all comments about this post from DB</li>
-       <li>add comment to DB</li>
-       <li>can edit the comment if written by login user </li>    
-     </ol>
-
-     <h3>The following parameters come to props: </h3>
-    <div>userID:props.uID={props.uID}</div>
-    <div>postID:props.pID ={props.pID}</div> */}
     </>
   );
 }
