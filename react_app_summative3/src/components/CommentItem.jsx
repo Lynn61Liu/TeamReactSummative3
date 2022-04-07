0b3f53dfa81513ce0c7d0e7e66727b3fc4eb7e8e
lynn <270146689@yoobeestudent.ac.nz>
2022-04-07 13:17:06 +1200

    user can delete self comments

--- react_app_summative3/src/components/CommentItem.jsx
+++ react_app_summative3/src/components/CommentItem.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Avatar from "@mui/material/Avatar";
-import mike from "../img/mike.jpg";
+import Cookies from 'js-cookie';
 
 function CommentItem(props) {
-  console.log('comment props=',props);
+  const [author, setauthor]=useState(Cookies.get('userID'));
+  const [isAuthor, setisAuthor]=useState(false);
+  const handeDeletCommet=()=>{
+    console.log('Cookies uid=', author);
+    console.log('props uid=', props.userID._id);
+    
+  }
   return (
     <div>
       <div className="commentsDone">
@@ -11,8 +17,8 @@ function CommentItem(props) {
         <div id="comment-box" className="postComment">
           <div className="comment-name">
             <span>{props.userID.userName}</span> 
+            {Cookies.get('userID') === props.userID._id ? <button onClick={handeDeletCommet} >DELETE </button> :""}
             
-            <button>DELETE</button>
           </div>
           {/* <div className="comment-time">AUG 25 2022 @ 14:33</div> */}
           <div className="comment-time">{props.createTime}</div>
